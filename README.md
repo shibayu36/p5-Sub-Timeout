cc9309b565a9fe7d47920db5ee180f40a0674639
shiba_yu36 <shibayu36@gmail.com>
2013-06-06 19:33:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sub::Timeout - It's new $module
+
+# SYNOPSIS
+
+    use Sub::Timeout;
+
+# DESCRIPTION
+
+Sub::Timeout is ...
+
+# LICENSE
+
+Copyright (C) shiba\_yu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+shiba\_yu36 <shibayu36@gmail.com>
---
+++ lib/Sub/Timeout.pm
@@ -0,0 +1,39 @@
+package Sub::Timeout;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::Timeout - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sub::Timeout;
+
+=head1 DESCRIPTION
+
+Sub::Timeout is ...
+
+=head1 LICENSE
+
+Copyright (C) shiba_yu36.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shiba_yu36 E<lt>shibayu36@gmail.comE<gt>
+
+=cut
+
